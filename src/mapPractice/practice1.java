edb5446450565717b44faf65fd6eec7cdc8b7949
mcheich <mcheich@gmail.com>
2020-01-16 19:52:07 -0500

    First Commit

---
+++ src/mapPractice/practice1.java
@@ -0,0 +1,15 @@
+package mapPractice;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		
+		List<String> myList = new ArrayList<String>();
+
+	}
+
+}
